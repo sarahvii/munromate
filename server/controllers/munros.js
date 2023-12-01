ec2714a7365a26954e20287c51888eac9d05cbed
Sarah Smith <sarahsmithvii@gmail.com>
2023-12-01 00:10:15 +0000

    installed express-async-errors library and removed try-catch blocks

--- server/controllers/munros.js
+++ server/controllers/munros.js
@@ -3,78 +3,67 @@ const Munro = require('../models/munro')
 
 // get all munros
 munrosRouter.get('/', async (request, response) => {
-    const munros = await Munro.find({})
-    response.json(munros)
-    });
+  const munros = await Munro.find({})
+  response.json(munros)
+});
 
 // get individual resource
-munrosRouter.get('/:id', async (request, response, next) => {
-    try {
-        const munro = await Munro.findById(request.params.id)
-        if (munro) {
-        response.json(munro)
-    } else {
-        response.status(404).end()
-        }
-    } catch(exception) {
-        next(exception)
-    }
+munrosRouter.get('/:id', async (request, response) => {
+  const munro = await Munro.findById(request.params.id)
+  if (munro) {
+    response.json(munro)
+  } else {
+    response.status(404).end()
+  }
 })
 
 // create new munro
-munrosRouter.post('/', async (request, response, next) => {
-    const body = request.body
+munrosRouter.post('/', async (request, response) => {
+  const body = request.body
 
-    if (body.name === undefined) {
-        return response.status(400).json({error: 'name missing'})
-    }
+  if (!body.name) {
+    return response.status(400).json({ error: 'name missing' })
+  }
 
-    const munro = new Munro({
-        name: body.name,
-        height: body.height,
-        near: body.near,
-        favourite: body.favourite || false,
-        description: body.description,
-        img: body.img,
-        })
-    try {
-    const savedMunro = await munro.save()
-    response.status(201).json(savedMunro)
-    } catch(exception) {
-        next(exception)
-    }
-    })
+  const munro = new Munro({
+    name: body.name,
+    height: body.height,
+    near: body.near,
+    favourite: body.favourite || false,
+    description: body.description,
+    img: body.img,
+  })
+
+  const savedMunro = await munro.save()
+  response.status(201).json(savedMunro)
+})
 
 // delete individual munro
-munrosRouter.delete('/:id', async (request, response, next) => {
-    try {
-        await Munro.findByIdAndRemove(request.params.id)
-        response.status(204).end();
-        } catch(exception) {
-            next(exception)
-            }
-        })  
+munrosRouter.delete('/:id', async (request, response) => {
+  await Munro.findByIdAndRemove(request.params.id)
+  response.status(204).end();
+})  
 
 // update a munro
-munrosRouter.put('/:id', (request, response, next) => {
-    const body = request.body
+munrosRouter.put('/:id', (request, response) => {
+  const body = request.body
+  
+  const munro = new Munro({
+    name: body.name,
+    height: body.height,
+    near: body.near,
+    favourite: body.favourite || false,
+    description: body.description,
+    img: body.img,
+  })
 
-    const munro = new Munro({
-        name: body.name,
-        height: body.height,
-        near: body.near,
-        favourite: body.favourite || false,
-        description: body.description,
-        img: body.img,
-        })
-
-    Munro.findByIdAndUpdate(request.params.id, munro, { new: true })
+  Munro.findByIdAndUpdate(request.params.id, munro, { new: true })
     .then(updatedMunro => {
-        if (updatedMunro) {
-            response.json(updatedMunro);
-        } else {
-            response.status(404).end();
-        }
+      if (updatedMunro) {
+        response.json(updatedMunro);
+      } else {
+        response.status(404).end();
+      }
     })
 })
 
--- server/tests/munro_api.test.js
+++ server/tests/munro_api.test.js
@@ -5,115 +5,114 @@ const app = require('../app')
 const api = supertest(app)
 
 const Munro = require('../models/munro')
-const munro = require('../models/munro')
 
 beforeEach(async () => {
-    await Munro.deleteMany({})
+  await Munro.deleteMany({})
 
-    let munroObject = new Munro(helper.initialMunros[0])
-    await munroObject.save()
+  let munroObject = new Munro(helper.initialMunros[0])
+  await munroObject.save()
 
-    munroObject = new Munro(helper.initialMunros[1])
-    await munroObject.save()
+  munroObject = new Munro(helper.initialMunros[1])
+  await munroObject.save()
 })
 
 test('munros are returned as json', async () => {
-    await api
-        .get('/api/munros')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+  await api
+    .get('/api/munros')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
 }, 100000)
 
 test('all munros are returned', async () => {
-    const response = await api.get('/api/munros')
+  const response = await api.get('/api/munros')
 
-    expect(response.body).toHaveLength(helper.initialMunros.length)
+  expect(response.body).toHaveLength(helper.initialMunros.length)
 })
 
 test('a specific munro is within the returned munros', async () => {
-    const response = await api.get('/api/munros')
+  const response = await api.get('/api/munros')
 
-    const contents = response.body.map(r => r.name)
-    expect(contents).toContain(
-        "Ben Macdui"
-    )
+  const contents = response.body.map(r => r.name)
+  expect(contents).toContain(
+    "Ben Macdui"
+  )
 })  
 
 test('a valid munro can be added', async () => {
-    const newMunro = {
-        "name": "Braeriach",
-        "height": 1296,
-        "near": "Cairngorms",
-        "favourite": false,
-        "description": "Braeriach or Brae Riach (Am Bràigh Riabhach) is the third-highest mountain in Scotland and all of the British Isles, after Ben Nevis and Ben Macdui, rising 1,296 metres (4,252 ft) above sea level. It is in the Scottish Highlands and is the highest point in the western massif of the Cairngorms, separated from the central section by the Lairig Ghru pass. The summit is a crescent-shaped plateau, overlooking several corries.",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Braeriach_and_An_Garbh_Choire_-_geograph.org.uk_-_278991.jpg"
-        }
+  const newMunro = {
+    "name": "Braeriach",
+    "height": 1296,
+    "near": "Cairngorms",
+    "favourite": false,
+    "description": "Braeriach or Brae Riach (Am Bràigh Riabhach) is the third-highest mountain in Scotland and all of the British Isles, after Ben Nevis and Ben Macdui, rising 1,296 metres (4,252 ft) above sea level. It is in the Scottish Highlands and is the highest point in the western massif of the Cairngorms, separated from the central section by the Lairig Ghru pass. The summit is a crescent-shaped plateau, overlooking several corries.",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Braeriach_and_An_Garbh_Choire_-_geograph.org.uk_-_278991.jpg"
+  }
     
-    await api
-        .post('/api/munros')
-        .send(newMunro)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+  await api
+    .post('/api/munros')
+    .send(newMunro)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
 
-    const munrosAtEnd = await helper.munrosInDb()
-    expect(munrosAtEnd).toHaveLength(helper.initialMunros.length + 1)
+  const munrosAtEnd = await helper.munrosInDb()
+  expect(munrosAtEnd).toHaveLength(helper.initialMunros.length + 1)
 
-    const names = munrosAtEnd.map(n => n.name)
-    expect(names).toContain(
-        'Braeriach'
-    )
+  const names = munrosAtEnd.map(n => n.name)
+  expect(names).toContain(
+    'Braeriach'
+  )
 })
 
 test('munro without name is not added', async() => {
-    const newMunro = {
-            "height": 1291,
-            "near": "Cairngorms",
-            "favourite": false,
-            "description": "Cairn Toul (Càrn an t-Sabhail) is the fourth-highest mountain in Scotland and all of the British Isles, after Ben Nevis, Ben Macdui and Braeriach. The summit is 1,291 metres (4,236 feet) above sea level. It is in the western massif of the Cairngorms, linked by a bealach at about 1125 m to Braeriach. The mountain towers above the Lairig Ghru pass.",
-            "img": "https://upload.wikimedia.org/wikipedia/commons/8/85/Cairn_Toul_-_geograph.org.uk_-_381800.jpg"
-            }
+  const newMunro = {
+    "height": 1291,
+    "near": "Cairngorms",
+    "favourite": false,
+    "description": "Cairn Toul (Càrn an t-Sabhail) is the fourth-highest mountain in Scotland and all of the British Isles, after Ben Nevis, Ben Macdui and Braeriach. The summit is 1,291 metres (4,236 feet) above sea level. It is in the western massif of the Cairngorms, linked by a bealach at about 1125 m to Braeriach. The mountain towers above the Lairig Ghru pass.",
+    "img": "https://upload.wikimedia.org/wikipedia/commons/8/85/Cairn_Toul_-_geograph.org.uk_-_381800.jpg"
+  }
     
-    await api
-        .post('/api/munros')
-        .send(newMunro)
-        .expect(400)
+  await api
+    .post('/api/munros')
+    .send(newMunro)
+    .expect(400)
 
-    const munrosAtEnd = await helper.munrosInDb()
-    expect(munrosAtEnd).toHaveLength(helper.initialMunros.length)
+  const munrosAtEnd = await helper.munrosInDb()
+  expect(munrosAtEnd).toHaveLength(helper.initialMunros.length)
 })
 
 test('a specific munro can be viewed', async () => {
-    const munrosAtStart = await helper.munrosInDb()
+  const munrosAtStart = await helper.munrosInDb()
 
-    const munroToView = munrosAtStart[0]
+  const munroToView = munrosAtStart[0]
 
-    const resultMunro = await api
-        .get(`/api/munros/${munroToView.id}`)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+  const resultMunro = await api
+    .get(`/api/munros/${munroToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
 
-    expect(resultMunro.body).toEqual(munroToView)
+  expect(resultMunro.body).toEqual(munroToView)
 })
 
 test('a munro can be deleted', async () => {
-    const munrosAtStart = await helper.munrosInDb()
-    const munroToDelete = munrosAtStart[0]
+  const munrosAtStart = await helper.munrosInDb()
+  const munroToDelete = munrosAtStart[0]
 
-    await api
-        .delete(`/api/munros/${munroToDelete.id}`)
-        .expect(204)
+  await api
+    .delete(`/api/munros/${munroToDelete.id}`)
+    .expect(204)
 
-    const munrosAtEnd = await helper.munrosInDb()
+  const munrosAtEnd = await helper.munrosInDb()
 
-    expect(munrosAtEnd).toHaveLength(
-        helper.initialMunros.length - 1
-    )
+  expect(munrosAtEnd).toHaveLength(
+    helper.initialMunros.length - 1
+  )
 
-    const names = munrosAtEnd.map(r => r.name)
-    expect(names).not.toContain(munroToDelete.name)
+  const names = munrosAtEnd.map(r => r.name)
+  expect(names).not.toContain(munroToDelete.name)
 })
 
 
 afterAll(async () => {
-    await mongoose.connection.close()
+  await mongoose.connection.close()
 })
